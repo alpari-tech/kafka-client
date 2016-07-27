e17f3931a864da169e71f3b9f098758d685edb70
Alexander Lisachenko <lisachenko.it@gmail.com>
2016-07-27 15:46:33 +0300

    Add ControlledShutdown API

---
+++ src/Kafka/Record/ControlledShutdownRequest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @author Alexander.Lisachenko
+ * @date 27.07.2014
+ */
+
+namespace Protocol\Kafka\Record;
+
+use Protocol\Kafka;
+use Protocol\Kafka\Record;
+
+/**
+ * This request asks for the controlled shutdown of specific broker
+ */
+class ControlledShutdownRequest extends AbstractRequest
+{
+    /**
+     * Broker identifier to shutdown
+     *
+     * @var integer
+     */
+    private $brokerId;
+
+    public function __construct($brokerId, $correlationId = 0, $clientId = '')
+    {
+        $this->brokerId = $brokerId;
+
+        parent::__construct(Kafka::CONTROLLED_SHUTDOWN, $correlationId, $clientId);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function packPayload()
+    {
+        $payload = parent::packPayload();
+
+        $payload .= pack('N', $this->brokerId);
+
+        return $payload;
+    }
+}
