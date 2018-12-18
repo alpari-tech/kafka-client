99fedbe882e1a9811182ea1bb0b97a891242ab5d
Alexander Lisachenko <lisachenko.it@gmail.com>
2018-12-18 16:16:47 +0300

    Drop unused constants and add modifier into Kafka class

--- src/Kafka.php
+++ src/Kafka.php
@@ -15,49 +15,25 @@ namespace Protocol;
 class Kafka
 {
     /**
-     * Number of bytes in a kafka header
-     */
-    const HEADER_LEN = 4;
-
-    /**
-     * Format of kafka header for unpacking in PHP
-     *
-     * RequestOrResponse => Size (RequestMessage | ResponseMessage)
-     * Size => int32
-     */
-    const HEADER_FORMAT = 'Nsize';
-
-    /**
-     * Format of kafka request header for unpacking in PHP
-     *
-     * Request Header => api_key api_version correlation_id client_id
-     *   api_key        => INT16
-     *   api_version    => INT16
-     *   correlation_id => INT32
-     *   client_id      => NULLABLE_STRING
-     */
-    const REQUEST_HEADER_FORMAT = 'napiKey/napiVersion/NcorrelationId/ZclientId';
-
-    /**
      * The following are the numeric codes that the ApiKey in the request can take for each of the below request types.
      */
-    const PRODUCE             = 0;
-    const FETCH               = 1;
-    const OFFSETS             = 2;
-    const METADATA            = 3;
-    const LEADER_AND_ISR      = 4;
-    const STOP_REPLICA        = 5;
-    const UPDATE_METADATA     = 6;
-    const CONTROLLED_SHUTDOWN = 7;
-    const OFFSET_COMMIT       = 8;
-    const OFFSET_FETCH        = 9;
-    const GROUP_COORDINATOR   = 10;
-    const JOIN_GROUP          = 11;
-    const HEARTBEAT           = 12;
-    const LEAVE_GROUP         = 13;
-    const SYNC_GROUP          = 14;
-    const DESCRIBE_GROUPS     = 15;
-    const LIST_GROUPS         = 16;
-    const SASL_HANDSHAKE      = 17;
-    const API_VERSIONS        = 18;
+    public const PRODUCE             = 0;
+    public const FETCH               = 1;
+    public const OFFSETS             = 2;
+    public const METADATA            = 3;
+    public const LEADER_AND_ISR      = 4;
+    public const STOP_REPLICA        = 5;
+    public const UPDATE_METADATA     = 6;
+    public const CONTROLLED_SHUTDOWN = 7;
+    public const OFFSET_COMMIT       = 8;
+    public const OFFSET_FETCH        = 9;
+    public const GROUP_COORDINATOR   = 10;
+    public const JOIN_GROUP          = 11;
+    public const HEARTBEAT           = 12;
+    public const LEAVE_GROUP         = 13;
+    public const SYNC_GROUP          = 14;
+    public const DESCRIBE_GROUPS     = 15;
+    public const LIST_GROUPS         = 16;
+    public const SASL_HANDSHAKE      = 17;
+    public const API_VERSIONS        = 18;
 }
